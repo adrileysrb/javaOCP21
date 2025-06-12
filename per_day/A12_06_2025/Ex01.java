d1c541c7348816be5ac4931fd1c211a8f7623dba
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2025-06-12 07:36:40 -0300

    manha 12_06_2025 - 1 exercicio

---
+++ per_day/A12_06_2025/Ex01.java
@@ -0,0 +1,26 @@
+package per_day.A12_06_2025;
+
+public class Ex01 {
+
+    public class A {
+
+    }
+
+    public static class B {
+        static class B1 {
+
+        }
+
+        static class B2 {
+
+        }
+    }
+
+    public static void main(String args[]) {
+        class C {
+
+        }
+
+    }
+
+}
